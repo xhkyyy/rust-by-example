7081bca202043dfc4640cf6c64783f59b0885d27
xhkyyy <tnndwc@gmail.com>
2018-12-29 23:11:02 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rust-by-example
+
+> https://www.rust-lang.org
+> https://www.rust-lang.org/learn
+> https://doc.rust-lang.org/stable/rust-by-example/ (使用左右按钮进行学习)
+
---
+++ hello.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // this is an example of a line comment
+
+    /*
+     *
+     * this is another type of comment, the block comment.
+     *
+     */
+
+    // println! is a macro
+    println!("hello World!");
+
+    let x = 5 + /* 90 */ 5;
+
+    println!("Is `x` 10 or 100? x={}", x);
+}
